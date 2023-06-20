2d11c4499faf9b146b86e5d0896f71db670224dc
tirthas16 <114461802+tirthas16@users.noreply.github.com>
2023-06-20 11:31:34 +0530

    Update streamlit_app.py
    Adding favourite menu with emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bluberry Oatmeal')
-streamlit.text('Kale, spinach & rocket smoothie')
-streamlit.text('Hard boied- range free eggs')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega 3 & Bluberry Oatmeal')
+streamlit.text('🥗 Kale, spinach & rocket smoothie')
+streamlit.text('🐔 Hard boied- range free eggs')
+streamlit.text('🥑🍞 Avocado toast')
  
